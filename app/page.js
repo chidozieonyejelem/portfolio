3f025e014c3b71da2c053075fbeb5f9cdd34fb54
Doz1E <chidozieonyejelem1@gmail.com>
2026-05-26 02:41:43 +0100

    Swap order: Experience before Personal Projects

--- app/page.js
+++ app/page.js
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <main className="mx-auto w-full max-w-[880px] flex-1 px-[clamp(20px,5vw,48px)]">
       <Hero />
-      <ProjectSection id="projects" label="Personal Projects" items={personalProjects} />
       <ProjectSection id="experience" label="Experience" items={experience} />
+      <ProjectSection id="projects" label="Personal Projects" items={personalProjects} />
       <TechStack />
       <AboutSection />
     </main>
